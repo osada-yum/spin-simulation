d11370fd42b590a504df1da0735229316a8b72fc
osada-yum <osada.yum@gmail.com>
2021-12-04 22:28:46 +0900

    Add `util_error_stop`.
    	modified:   src/utility_m.f90

--- src/utility_m.f90
+++ src/utility_m.f90
@@ -4,4 +4,15 @@ module utility_m
   integer    , parameter :: ikind = int32, rkind = real64
   real(rkind), parameter :: epsilon = 1e-7_rkind
 contains
+
+  subroutine util_error_stop(message, linum, filename)
+    integer, intent(in) :: linum
+    character(len=*)    :: message, filename
+    write(error_unit, '(a, i0)') "error in "//filename//":", linum
+    write(error_unit, '(a)'    ) message
+#ifdef DEBUG
+    error stop 1
+#endif
+  end subroutine util_error_stop
+
 end module utility_m
