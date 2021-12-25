496ebae12d1c55cb5679fc11210d49e75e3fa3d0
osada-yum <osada.yum@gmail.com>
2021-12-25 21:16:20 +0900

    Use [...] instead of (/.../).
    	modified:   src/model/Ising2d_m.f90

--- src/model/Ising2d_m.f90
+++ src/model/Ising2d_m.f90
@@ -8,8 +8,8 @@ module Ising2d_m
   integer, parameter, private :: num_neighbors = 4                          ! 最近接格子数.
   integer, parameter, private :: J_interaction = 1.0
   integer, parameter, private :: array_discrete_energy_diff(-8:8) = &
-       (/-8, -7, -6, -5, -4, -3, -2, -1, 0&
-       ,  1,  2,  3,  4,  5,  6,  7,  8/)
+       [-8, -7, -6, -5, -4, -3, -2, -1, 0&
+       , 1,  2,  3,  4,  5,  6,  7,  8]
 
   type Ising2d
      private
